11561d3aa15ef76b39c62bcc2aae01f42c6cc8d3
liuly <me@liuly.moe>
2023-06-27 19:59:12 +0800

    refactor: delete with transaction

--- server/api/papers.delete.ts
+++ server/api/papers.delete.ts
@@ -1,16 +1,15 @@
 export default defineEventHandler(async (event) => {
   const { id } = await readBody(event)
-  // 删除指定 id 的文章
-  // 注意需要首先级联删除 TeacherOnPaper 表
-  await event.context.prisma.teacherOnPaper.deleteMany({
-    where: {
-      paperId: id
-    }
-  })
-  await event.context.prisma.paper.delete({
-    where: {
-      id
-    }
-  })
-  return true
+  return await event.context.prisma.$transaction([
+    event.context.prisma.teacherOnPaper.deleteMany({
+      where: {
+        paperId: id
+      }
+    }),
+    event.context.prisma.paper.delete({
+      where: {
+        id
+      }
+    })
+  ])
 })
