71f03ddda765f2dd40c239b5a48bf25af56d1803
liuly <me@liuly.moe>
2023-06-27 19:40:53 +0800

    refactor: insert in a more orm way

--- server/api/papers.post.ts
+++ server/api/papers.post.ts
@@ -1,27 +1,21 @@
 export default defineEventHandler(async (event) => {
   const { name, origin, year, type, level, authors, communicationAuthor } = await readBody(event)
-
-  const paper = await event.context.prisma.paper.create({
+  return await event.context.prisma.paper.create({
     data: {
       name,
       origin,
       year: Number(year),
       type,
-      level
+      level,
+      TeacherOnPaper: {
+        createMany: {
+          data: authors.map((authorId: string, index: number) => ({
+            teacherId: authorId,
+            is_communicating_author: authorId === communicationAuthor,
+            rank: index + 1
+          }))
+        }
+      }
     }
   })
-
-  const teacherOnPaper = await event.context.prisma.teacherOnPaper.createMany({
-    data: authors.map((authorId: string, index: number) => ({
-      paperId: paper.id,
-      teacherId: authorId,
-      is_communicating_author: authorId === communicationAuthor,
-      rank: index + 1
-    }))
-  })
-
-  return {
-    paper,
-    teacherOnPaper
-  }
 })
